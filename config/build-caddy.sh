491b1be5071781b51065957dfe7af895e49d9cd1
Cory Cooper <coopercory92@gmail.com>
2022-12-02 13:39:17 -0700

    Plugin pipeline with tarball

---
+++ config/build-caddy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+eval APPMASKER_ACCESS_TOKEN="$1"
+eval ACCESS_TOKEN_USR="$2"
+eval ACCESS_TOKEN_PWD="$3"
+eval PLUGIN_REPOS="$4"
+
+git clone "https://oauth2:${APPMASKER_ACCESS_TOKEN}@github.com/appmasker/caddy-admin-repeat" --single-branch --depth 1
+git clone "https://oauth2:${APPMASKER_ACCESS_TOKEN}@github.com/appmasker/caddy_rest_storage" --single-branch --depth 1
+
+getPluginReposFlags() {
+  REPOFLAGS=""
+  for repo in ${PLUGIN_REPOS//,/ }
+    do
+      REPOFLAGS+=" --with ${repo}"
+    done
+  echo $REPOFLAGS
+}
+
+eval "xcaddy build v2.6.0 \
+  $(getPluginReposFlags) \
+  --with github.com/appmasker/caddy-admin-repeat=./caddy-admin-repeat \
+  --with github.com/appmasker/caddy_rest_storage=./caddy_rest_storage"
